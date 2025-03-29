dda3dd4deec5b785f541a441d13b2a07823fe908
Muskantam26 <mskntmrkr@gmail.com>
2025-03-29 16:11:59 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+
+// ======================================Day 1=============================================================
+
+// const name= "muskan"
+// const age=10
+// const salary=34000
+
+// const table= <ul>
+//   <li>php</li>
+//   <li>java</li>
+//   <li>mern</li>
+//   <li>js</li>
+//   <li>java</li>
+// </ul>
+  
+
+// =========================================Day 2==================================================================================
+
+// import Cybrom from "./cybrom";
+import Header from "./Header";
+import Data from "./Data";
+import Footer from "./footer";
+
+const App=()=>{
+  return(
+    // <h1> Welcome To react class! {table}</h1>
+    // <>
+    // <h1>welcome</h1>
+    // <h1>to react class</h1>
+    // </>
+
+
+    <>
+    {/* Enter name: <input type="text" /><br /><br />
+    Enter city : <input type="text" />
+    <Cybrom/> */}
+
+    <Header/>
+    <Data/>
+    <Footer/>
+    
+    </>
+  )
+}
+export default App;
